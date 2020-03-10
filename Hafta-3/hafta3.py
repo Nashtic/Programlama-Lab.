8feb27ec4dad72e72fb3d84bda1c84550e49761d
Nashtic <44553752+Nashtic@users.noreply.github.com>
2020-03-10 18:05:32 +0300

    Update Hafta3

--- Hafta-3/hafta3.py
+++ Hafta-3/hafta3.py
@@ -1,5 +1,3 @@
-#Selahattin Ahmed Ataşoğlu Hafta 3 Yazılan Kodlar
-
 list_1 = [0,5,25,100,5,5,0,100]
 
 def my_h(list_1):
@@ -19,7 +17,14 @@ def lookup(d,v):
             return key
         else:
             return -1
+
+my_d =dict()
+
+my_d ={1:3,2:5,6:8}
+
 lookup(my_d,1)
+
+
 
 #Fibonacci Recursive
 n = int(input())
@@ -41,19 +46,3 @@ def fibo_rec(n):
         result=fibo_rec(n-1)+fibo_rec(n-2)
         known[n]=result
         return result
-
-from sympy import FiniteSet
-
-s=FiniteSet(1,1.5,Fraction(1,5)) #aynı şeyleri tekrar yazsan da yazmaz çünkü küme elemanlarını yazıyor.
-for member in s:
-    print(member)
-
-t=FiniteSet(Fraction(1,5),1.5,1,1)
-if s==t:
-    print("True")
-
-
-list_1=[1,4,7,84,3,62,23]
-my_d=dict()
-my_d{1:"bir",2:2,"2":"three"}
-print(my_d)
